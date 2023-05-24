b5cef20755f6176a2da23804d38f43088ecdfc44
Nirmal Patel <npatel@horizontal.com>
2023-05-24 13:40:58 +0530

    frontend init of react routes

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,8 +1,12 @@
+
 import React from 'react'
+import PageContent from '../components/PageContent'
 
 const HomePage = () => {
   return (
-    <h1>HomePage</h1>
+    <PageContent title="Welcome!">
+      <p>Browsing through all our events!</p>
+    </PageContent>
   )
 }
 
